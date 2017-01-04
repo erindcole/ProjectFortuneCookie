3fdf329ced7eaf44fb3de3e1f3ff02034acd3251
Erin D Cole <edcole@ieng6-203.ucsd.edu>
2017-01-03 18:10:08 -0800

    started FCTrie class and its unit test

---
+++ FCTrie.java
@@ -0,0 +1,9 @@
+/* Names: Erin Cole and Christina Cook
+ * Filename: FCTrie.java
+ * Description: This class is responsible for building the trie that connects
+ * 		the fortune databases.
+ */
+
+class FCTrie {
+
+}
---
+++ test_FCTrie.java
@@ -0,0 +1,13 @@
+/* Names: Erin Cole and Christina Cook
+ * Filename: test_FCTrie.java
+ * Description: unit test for FCTrie.java
+ */
+
+
+class test_FCTrie {
+
+
+void main() {
+
+}
+}
