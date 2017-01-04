c64d6eb9154c467467c1eebe45d0c19ea58e74af
Erin D Cole <edcole@ieng6-203.ucsd.edu>
2017-01-03 17:25:29 -0800

    Wrote notes to Christina to help her get coding on PFC.java

--- PFC.java
+++ PFC.java
@@ -22,6 +22,11 @@ public static void main(String[] args) {
  * 	    need to return it
  *
  * Keep main small, allow the work to be done underneath
+ * Basically this function needs to process the user's selection of clicking on  * the fortune, the connection between the UI and the Java code.n After processi * ng the user input, it will call  a function in the class Fortune to start the * assembly of the fortune text.
+ *
+ * I know we need Joel's help to connect the front end and the back end.  Do as  * much as you can of this without actually connecting the two. Pretend you have * connected it and act as though the inputs to the function calls have magicall * y been created in the functions from which they are called.  We need to get t * his part hashed out as concretely as possible with the understanding that we  * don't have all the dots connected yet.
+ *
+ * Please message me if anything isn't clear or if you have ideas to make this f * ile better! These are merely my ideas and I am interested in knowing your tak * e :) 
  */
 
 }
