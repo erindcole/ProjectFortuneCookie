9e7690f2040011d6d95577fc8a7befdfdcd59aac
Erin D Cole <edcole@ieng6-201.ucsd.edu>
2017-01-08 17:57:32 -0800

    started writing test cases for FCTrie class

--- test_FCTrie.java
+++ test_FCTrie.java
@@ -6,8 +6,22 @@
 
 class test_FCTrie {
 
+void test_insert(String[] trieNode) {
+       
+     FCTrie myTrie = new FCTrie(null);
+     myTrie.insert(trieNode);
+}
+
+void test_find(String[] trieNode) {
+     FCTrie findTrie = new FCTrie(null);
+     findTrie.find(trieNode);
+}
+
+int databaseSize = 40;
+String[] trieNode = new String[databaseSize];
 
 void main() {
-
+     test_insert(trieNode);         
+     test_find(trieNode);
 }
 }
